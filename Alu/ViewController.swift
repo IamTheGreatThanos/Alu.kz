03edbc5c541ce7b459cb51d43dba5d8f2c8bb0a0
Dinmukhammed <iamthegreatthanos@gmail.com>
2020-07-31 23:29:56 +0600

    Initial Commit

---
+++ Alu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alu
+//
+//  Created by THANOS on 7/31/20.
+//  Copyright © 2020 XCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
